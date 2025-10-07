904a8740317f4bc6ef461351ddf6b6744a4a67ae
juansilva29 <hammerpipo21@hotmail.com>
2025-10-07 16:51:49 -0500

    se aplica patron de estructura page object model

---
+++ tests/login.spec.ts
@@ -0,0 +1,21 @@
+import { test, expect } from '@playwright/test';
+import { LoginPage } from '../pages/LoginPage';
+import { DashboardPage } from '../pages/dashboardPage';
+import TestData from '../data/testData.json';
+
+let loginPage: LoginPage;
+let dashboardPage: DashboardPage;
+
+test.beforeEach(async ({ page }) => {
+    loginPage = new LoginPage(page);
+    dashboardPage = new DashboardPage(page);
+    await loginPage.visitLoginPage();
+});
+
+test('TC-1 verify login success', async ({ page }) => {
+    await test.step('Completar el login y verificar mensaje de éxito', async () => {
+        await loginPage.completeAndSubmitLogin(TestData.usuarioExistente);
+        await expect(page.getByText('Inicio de sesión exitoso')).toBeVisible();
+        await expect(dashboardPage.dashboardTitle).toBeVisible();
+    });
+});

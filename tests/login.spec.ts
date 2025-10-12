49fbf636830dee9fc3160871481753646b40684b
juansilva29 <hammerpipo21@hotmail.com>
2025-10-12 09:58:57 -0500

    Actualizacion de casos de pruerba login desde backend

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -2,6 +2,7 @@ import { test, expect } from '@playwright/test';
 import { LoginPage } from '../pages/LoginPage';
 import { DashboardPage } from '../pages/dashboardPage';
 import TestData from '../data/testData.json';
+import { PassThrough } from 'stream';
 
 let loginPage: LoginPage;
 let dashboardPage: DashboardPage;
@@ -24,12 +25,29 @@ test('TC-2 verify login from backend', async ({ page, request }) => {
     const email = 'Juan' + Date.now().toString() + '@test.com';
     TestData.usuarioValido.email = email;
     const response = await request.post('http://localhost:6007/api/auth/signup', {
+        data: {
+            firstName: TestData.usuarioValido.nombre,
+            lastName: TestData.usuarioValido.apellido,
+            email: email,
+            password: TestData.usuarioValido.password,
+        }
     });
 
-    const responseBody = await response.json();
     expect(response.status()).toBe(201);
 
-    await loginPage.completeAndSubmitLogin(TestData.usuarioExistente);
+    const responsePromiseLogin = page.waitForResponse('http://localhost:6007/api/auth/login');
+    await loginPage.completeAndSubmitLogin({ email: email, password: TestData.usuarioValido.password });
+    const responseLogin = await responsePromiseLogin;
+    const reponseBodyLogin = await responseLogin.json();
+    expect(responseLogin.status()).toBe(200);
+    expect(reponseBodyLogin).toHaveProperty('token');
+    expect(reponseBodyLogin).toHaveProperty('user');
+    expect(reponseBodyLogin.user).toEqual(expect.objectContaining({
+        id: expect.any(String),
+        firstName: TestData.usuarioValido.nombre,
+        lastName: TestData.usuarioValido.apellido,
+        email: email,
+    }));
     await expect(page.getByText('Inicio de sesión exitoso')).toBeVisible();
     await expect(dashboardPage.dashboardTitle).toBeVisible();
 });
